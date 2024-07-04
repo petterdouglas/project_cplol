92651c8b1925152a7c40be834524d62d01dae636
Petter <petterdouglas7@gmail.com>
2024-07-04 12:44:55 -0300

    styles: refactoring some styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { FaBars } from 'react-icons/fa'
 
 import '../styles/components/navbar.sass'
 
+import logo from '../assets/cplol-icon.webp'
+
 const Navbar = ({ setBar, nonActive }) => {
 
   const showSidebar = () => {
@@ -12,7 +14,7 @@ const Navbar = ({ setBar, nonActive }) => {
   return (
     <nav>
       <div className="title_container">
-        <img className='logo' src="src/assets/cplol-icon.webp" alt="" />
+        <img className='logo' src={logo} alt="" />
         <NavLink className='title' to='/'>CPLOL</NavLink>
       </div>
       {nonActive ? null : <FaBars className='bar' onClick={showSidebar} />}
