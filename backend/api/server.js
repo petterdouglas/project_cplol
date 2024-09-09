38ccbe0ae82b389e1edc60420b17b380ba6f1077
Petter <petterdouglas7@gmail.com>
2024-09-09 04:52:08 -0300

    some changes

--- backend/api/server.js
+++ backend/api/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cors({
-    origin: 'http://localhost:5173', // Ou o endereço da sua aplicação frontend
+    origin: 'https://project-cplol.vercel.app/', // Ou o endereço da sua aplicação frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
 }))
