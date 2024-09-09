505487f3af442a6180630ae4ecf84a9ce7c62224
Petter <petterdouglas7@gmail.com>
2024-09-09 05:15:01 -0300

    .

--- backend/api/server.js
+++ backend/api/server.js
@@ -41,48 +41,4 @@ app.post('/form', async (req, res) => {
     res.status(201).json(req.body)
 })
 
-app.get('/form', async (req, res) => {
-    const teams = await prisma.team.findMany()
-    res.status(200).json(teams)
-})
-
-app.put('/form/:id', async (req, res) => {
-    await prisma.team.update({
-        where: {
-            id: req.params.id
-        },
-        data: {
-            email: req.body.email,
-            name: req.body.name,
-            leadNick: req.body.leadNick,
-            name1: req.body.name1,
-            member1: req.body.member1,
-            position1: req.body.position1,
-            name2: req.body.name2,
-            member2: req.body.member2,
-            position2: req.body.position2,
-            name3: req.body.name3,
-            member3: req.body.member3,
-            position3: req.body.position3,
-            name4: req.body.name4,
-            member4: req.body.member4,
-            position4: req.body.position4,
-            name5: req.body.name5,
-            member5: req.body.member5,
-            position5: req.body.position5,
-            icon: req.body.icon
-        }
-    })
-    res.status(202).json(req.body)
-})
-
-app.delete('/form/:name', async (req, res) => {
-    await prisma.team.delete({
-        where: {
-            name: req.params.name
-        }
-    })
-    res.status(200).json({ message: "Time deletado com sucesso!" })
-})
-
 app.listen(port)
