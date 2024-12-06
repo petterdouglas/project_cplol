6e3a97005d27a40d9aa8187f5f7bdb2a95d2b2c1
Petter <petterdouglas7@gmail.com>
2024-12-06 01:12:14 -0300

    new schedule

--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -6,19 +6,15 @@ const Schedule = () => {
       <h1 className='schedule_contents-title'>Programação</h1>
       <ul>
         <li>
-          <p className='schedule_contents-dates'>5/12 a 11/12</p>
+          <p className='schedule_contents-dates'>15/12 a 20/12</p>
           <p className='schedule_contents-case'>Eliminatórias</p>
         </li>
         <li>
-          <p className='schedule_contents-dates'>12/12 a 13/12</p>
-          <p className='schedule_contents-case'>Quartas de final</p>
-        </li>
-        <li>
-          <p className='schedule_contents-dates'>14/12</p>
+          <p className='schedule_contents-dates'>21/12</p>
           <p className='schedule_contents-case'>Semifinais</p>
         </li>
         <li>
-          <p className='schedule_contents-dates'>15/12</p>
+          <p className='schedule_contents-dates'>22/12</p>
           <p className='schedule_contents-case'>Final</p>
         </li>
       </ul>
