0c32d17bdb5d5638cfdea75c73b1ca96cba4200e
Petter <petterdouglas7@gmail.com>
2024-12-10 20:49:59 -0300

    changes: data na Home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,9 +4,9 @@ import '../styles/components/home.sass'
 const Home = () => {
   return (
     <div className="home_contents">
-      <h2 className='home-edition'>1ª edição</h2>
+      <h2 className='home-edition'>2ª edição</h2>
       <h1 className='home-title'>cplol</h1>
-      <h2 className='home-date'>dia 10/12</h2>
+      <h2 className='home-date'> 15/12</h2>
       <NavLink className='home-btn' to='form'>Inscreva-se</NavLink>
     </div>
   )
