80d8829eee1821c14248c26d35f63b9df9ae6485
Petter <petterdouglas7@gmail.com>
2024-12-22 17:48:02 -0300

    add: new schedule

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="home_contents">
       <h2 className='home-edition'>2ª edição</h2>
       <h1 className='home-title'>cplol</h1>
-      <h2 className='home-date'> 15/12</h2>
+      <h2 className='home-date'> 14/01 a 21/01</h2>
       <NavLink className='home-btn' to='form'>Inscreva-se</NavLink>
     </div>
   )
--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -6,15 +6,15 @@ const Schedule = () => {
       <h1 className='schedule_contents-title'>Programação</h1>
       <ul>
         <li>
-          <p className='schedule_contents-dates'>15/12 a 20/12</p>
+          <p className='schedule_contents-dates'>14/01 a 19/01</p>
           <p className='schedule_contents-case'>Eliminatórias</p>
         </li>
         <li>
-          <p className='schedule_contents-dates'>21/12</p>
+          <p className='schedule_contents-dates'>20/01</p>
           <p className='schedule_contents-case'>Semifinais</p>
         </li>
         <li>
-          <p className='schedule_contents-dates'>22/12</p>
+          <p className='schedule_contents-dates'>21/01</p>
           <p className='schedule_contents-case'>Final</p>
         </li>
       </ul>
